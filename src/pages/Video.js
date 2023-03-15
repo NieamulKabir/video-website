eb3168f3a52809247e010d470a687406089c4bcf
NieamulKabir <kabir000079@gmail.com>
2023-03-15 22:24:25 +0600

    update video description

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import Player from '../components/description/Player';
 import VideoDescription from '../components/description/VideoDescription';
 import RelatedVideoList from '../components/list/RelatedVideoList';
+import { fetchVideo } from '../features/video/videoSlice';
+import Loading from '../utils/Loading';
 
+
+
+// if (!isLoading && !isError && videos?.length > 0) {
+//     content = 
+//         videos.map(video=>( <VideoGridItem  key={video.id} video={video} />))
 
 const Video = () => {
+    const dispatch = useDispatch();
+    const { video, isLoading, isError, error } = useSelector((state) => state.video)
+    const { videoId } = useParams()
+
+    useEffect(() => {
+        dispatch(fetchVideo(videoId))
+    }, [dispatch, videoId])
+
+    const { id, link, title, tags } = video || {}
+
+    let content = null;
+    if (isLoading) content = <Loading />
+    if (!isLoading && isError) content = <div className="col-span-12">{error}</div>
+    if (!isLoading && !isError && !video?.id) content = <div className="col-span-12">No video Found...</div>
+
+    if (!isLoading && !isError && video?.id) {
+        content =
+            <div className="grid grid-cols-3 gap-2 lg:gap-8">
+                <div className="col-span-full w-full space-y-8 lg:col-span-2">
+
+                    <Player link={link} title={title} />
+
+                    <VideoDescription video={video} />
+                </div>
+
+                <RelatedVideoList currentVideoId={id} tags={tags} />
+            </div>
+    }
+
     return (
         <div>
-       
+
             <section className="pt-6 pb-20">
                 <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-                    <div className="grid grid-cols-3 gap-2 lg:gap-8">
-                        <div className="col-span-full w-full space-y-8 lg:col-span-2">
-
-                            <Player />
-
-                            <VideoDescription />
-                        </div>
-
-                        <RelatedVideoList />
-                    </div>
+                    {content}
                 </div>
             </section>
-      
+
         </div>
     );
 };
