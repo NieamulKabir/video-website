7a0ba4fefc1df69bba56e8e5150a24b35590a248
NieamulKabir <kabir000079@gmail.com>
2023-03-15 19:46:31 +0600

    add videosSlice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Video from './pages/Video';
 
 
 function App() {
   return (
-    <Home></Home>
+    <Router>
+      <Navbar></Navbar>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:videoId" element={<Video />} />
+          
+      </Routes>
+      <Footer></Footer>
+    </Router>
   );
 }
 
